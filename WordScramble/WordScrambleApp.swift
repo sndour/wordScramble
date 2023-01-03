d59b0d11c5fd321251c8d22107825d7d9869e8eb
sndour <ndouradefemi@gmail.com>
2023-01-03 20:52:38 +0000

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Stéphane Adefemi NDOUR on 03/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
